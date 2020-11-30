dedce6e91783fe132e13bff9b6dccac90dfd03a3
Horlando Leão <horlandoleao8@gmail.com>
2020-11-29 21:32:12 -0300

    modelo treinado corretamente, menos overfitado

--- opencv-tensorflow/modelo-ia-recon-img/KERAS/recon_pessoas_armas.py
+++ opencv-tensorflow/modelo-ia-recon-img/KERAS/recon_pessoas_armas.py
@@ -6,24 +6,25 @@ import numpy as np
 #np.set_printoptions(suppress=False)
 np.set_printoptions(suppress=True)
 # Load the model
-model = tensorflow.keras.models.load_model('keras_model.h5', compile=False)
+model = tensorflow.keras.models.load_model('keras_model-2.h5', compile=False)
 
 # Crie a matriz da forma certa para alimentar o modelo keras
 # O 'comprimento' ou número de imagens que você pode colocar no array é
 # determinado pela primeira posição na tupla de forma, neste caso 1.
-data = np.ndarray(shape=(1, 600, 600, 3), dtype=np.float32)
+data = np.ndarray(shape=(1, 300, 300, 3), dtype=np.float32)
 
 # IMAGEM DE ARMAS
-image = Image.open('fotos/armas/armas (2).jpg.rf.726496ca5e4a8e90337f56332fe00432.jpg')#1
-#image = Image.open('fotos/armas/armas (15).jpg.rf.f52633782a3100f93d122151463773ca.jpg')#2
+#image = Image.open('fotos/com-arma/pessoa-6.jpg')#1 -   [[0.00000107 0.9999989 ]];[0.9983941  0.00160587]
+#image = Image.open('fotos/com-arma/pessoa-10.jpg')#2 - [[0. 1.]];                 [0.00000724 0.9999927 ]
+image = Image.open('fotos/com-arma/pessoa-15.jpg')#3 -     NULL  ;                  [0.72943753 0.2705625 ]
 
 # IMAGEM DE PESSOAS
-#image = Image.open('fotos/pessoas/image_59.jpg')#3
-#image = Image.open('fotos/pessoas/image_64.jpg')#4
+#image = Image.open('fotos/sem-arma/image_64.jpg')#3    [[0. 1.]];                 [0.00086649 0.9991335 ]
+#image = Image.open('fotos/sem-arma/image_82.jpg')#4    [[0. 1.]];                 [0.00000088 0.99999917]
 
 #redimensionar a imagem para 224x224 com a mesma estratégia do TM2:
 #redimensionar a imagem para pelo menos 224 x 224 e depois cortar do centro
-size = (600, 600)
+size = (300, 300)
 image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
 # transforme a imagem em um array numpy
@@ -34,21 +35,13 @@ image.show()
 
 # Normaliza a imagem
 #normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-normalized_image_array = (image_array.astype(np.float32) / 10.0) - 1
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 # Carregue a imagem no array
 data[0] = normalized_image_array
 
 # execute a inferência
 prediction = model.predict(data)
-print(prediction)
+print(prediction[0])
 
 
-
-"""
-a = (model.predict(X_test)>0.5).astype(int).ravel()
-print(a)
-
-reverse_pred = label_enc.inverse_transform(a.ravel())
-print(reverse_pred)
-"""
 
