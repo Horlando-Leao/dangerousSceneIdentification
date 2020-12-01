7f3465ce23b77b10a9d57f8e9aced3d978f28259
Horlando Leão <horlandoleao8@gmail.com>
2020-12-01 03:58:58 -0300

    hands

--- src/controllers/handiling.py
+++ src/controllers/handiling.py
@@ -1,4 +1,12 @@
 from detects.detectionFaces import detection_faces
-from detects.detectionGuns import detection_guns
-from detects.detectionBody import detection_body
+from detects.detectionGuns import  detection_guns
+from detects.detectionBody import  detection_body
 
+def process_img():
+    return True
+
+def set_weights():
+    return True
+
+def handiling():
+    return True
