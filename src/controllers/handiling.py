7ba592baa02d3d16be69fc469cd6bbf4776b5ba8
Horlando Leão <horlandoleao8@gmail.com>
2020-12-02 13:05:07 -0300

    Update handiling.py

--- src/controllers/handiling.py
+++ src/controllers/handiling.py
@@ -2,11 +2,23 @@ from detects.detectionFaces import detection_faces
 from detects.detectionGuns import  detection_guns
 from detects.detectionBody import  detection_body
 
-def process_img():
-    return True
+def handiling(url_imagem: str) -> json:
 
-def set_weights():
-    return True
+    def process_img(url_imagem: str) -> array:
+        try:
+            face = detection_faces(url_imagem)
+            body = detection_body(url_imagem)
+            gun  = detection_guns(url_imagem)
+            return [face, body, gun]
 
-def handiling():
-    return True
+        except Exception as erro:
+            print("erro: ", erro)
+            return[0]
+
+    def set_weights():
+        return True
+
+    def handiling():
+        return True
+    
+    return true
