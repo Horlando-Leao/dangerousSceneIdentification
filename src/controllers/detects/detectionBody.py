2dc89f30a95cd2b5ece92282d62f9d3f858252e1
Horlando Leão <horlandoleao8@gmail.com>
2020-12-02 23:27:37 -0300

    melhorando a precisão do upper body

--- src/controllers/detects/detectionBody.py
+++ src/controllers/detects/detectionBody.py
@@ -3,30 +3,43 @@ from utils import url_to_image_array
 from PIL import Image, ImageFilter
 
 
-def detection_body(imagem_url: str) -> int:
+def detection_bodys(imagem_url: str) -> int:
+    """RECEBE UMA URL DE IMAGEM, E RETORNA A QUANTIDADE 
+    DE PARTES SUPERIORES DAS PESSOAS NA FOTO"""
+
     carregaAlgoritmo = cv2.CascadeClassifier("Haarcascade/haarcascade_upperbody.xml")
 
     imagem = url_to_image_array(imagem_url)
 
-    #imagem = cv2.imread('caminho')
-    imagem = cv2.resize(imagem, (0,0), fx=0.7, fy=0.7) 
+    #=============================================
+
+    scale_percent = 120 # percent of original size
+    width = int(imagem.shape[1] * scale_percent / 100)
+    height = int(imagem.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+    imagem = cv2.resize(imagem, dim, interpolation = cv2.INTER_AREA)
+
+    #=============================================
+    #imagem = cv2.resize(imagem, (0,0), fx=1, fy=1) 
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
     #DETECÇÃO DAS bodys
     bodys = carregaAlgoritmo.detectMultiScale(
         imagemCinza, 
         scaleFactor=1.1, 
-        minNeighbors=1,  #abordagem de vizihança, (^) = + perder os verdadeiros positivos, (v) = + falsos positivos
-        minSize=(50,20) #tamanho da detecção de uma face
+        #minNeighbors=3,  #abordagem de vizihança, (^) = + perder os verdadeiros positivos, (v) = + falsos positivos
+        minSize=(1,1),
+        maxSize=(50,50) 
         )
 
     ##crie um retangulo nas bodys que detectMultiScale localizou
-    """ for(x, y, l, a) in bodys:
-        cv2.rectangle( imagem, ( x , y ), ( x + l, y + a ),( 0, 255, 0 ), 2 ) """
+    for(x, y, l, a) in bodys:
+        cv2.rectangle( imagem, ( x , y ), ( x + l, y + a ),( 0, 255, 0 ), 2 )
 
     ##exibe a imagem
-    #cv2.imshow("bodys", imagem)
-    #cv2.waitKey()
+    cv2.imshow("bodys", imagem)
+    cv2.waitKey()
 
     #Pequena regra de negócio
     count = 0
@@ -41,3 +54,7 @@ def detection_body(imagem_url: str) -> int:
         print("Erro: ", EOFError)
         return (count)
 
+
+url = "https://www.tvsaj.com.br/wp-content/uploads/2020/06/Pessoa-em-situa%C3%A7%C3%A3o-de-rua-resgatada-pela-prefeitura-de-santo-antonio-de-jesus.jpeg"
+url2 = "https://amenteemaravilhosa.com.br/wp-content/uploads/2018/09/homem-andnaod-e-mexendo-no-celular.jpg"
+print(detection_bodys(url))
--- src/controllers/handiling.py
+++ src/controllers/handiling.py
@@ -1,6 +1,6 @@
 from detects.detectionFaces import detection_faces
 from detects.detectionGuns import  detection_guns
-from detects.detectionBody import  detection_body
+from detects.detectionBody import  detection_bodys
 from detects.reconFacial import reconginit_face
 
 def handiling(url_imagem: str) -> str:
@@ -8,7 +8,7 @@ def handiling(url_imagem: str) -> str:
     def process_img(url_imagem: str):
         try:
             face = detection_faces(url_imagem)#int 0 até ...
-            body = detection_body(url_imagem)#int 0 até ...
+            body = detection_bodys(url_imagem)#int 0 até ...
             gun  = detection_guns(url_imagem)#array 2 posições
             return [face, body, gun[0], gun[1]]
 
